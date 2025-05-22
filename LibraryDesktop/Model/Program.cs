f4a43036f893786dd5ed5d927322f68a4ca75a87
quynh2204 <phuongquynh0204cm@gmail.com>
2025-05-23 02:33:25 +0700

    Add project files.

---
+++ LibraryDesktop/Model/Program.cs
@@ -0,0 +1,19 @@
+using LibraryDesktop.View; // Add this using directive
+
+namespace LibraryDesktop.Model
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Main());
+        }
+    }
+}
